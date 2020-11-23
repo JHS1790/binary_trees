d0983a96ae33538f34cb45ef32439848567c3d0b
JHS1790 <1790@holbertonschool.com>
2020-11-23 15:36:57 +0000

    temporarily removed problomatic prototypes

--- binary_trees.h
+++ binary_trees.h
@@ -52,8 +52,10 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 
 /* Non-Project Prototypes */
+/*
 static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
 static size_t _height(const binary_tree_t *tree);
+*/
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* B_TREES */
