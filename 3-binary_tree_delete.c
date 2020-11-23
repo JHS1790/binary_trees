f213510fc70f6664224ce4cabe7ab56e10b80e5d
JHS1790 <1790@holbertonschool.com>
2020-11-23 16:38:29 +0000

    betty check, removed erroneus space

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -23,7 +23,7 @@ void binary_tree_delete(binary_tree_t *tree)
 				if (nav_node->parent->right == nav_node)
 					nav_node->parent->right = NULL;
 			}
-			free (nav_node);
+			free(nav_node);
 			if (nav_node == tree)
 				tree = NULL;
 			else
