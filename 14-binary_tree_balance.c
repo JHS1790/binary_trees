89fe3b81270c0b89d420419672c02c7f3b266916
JHS1790 <1790@holbertonschool.com>
2020-11-24 16:45:52 +0000

    first go at 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,57 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the balance factor
+ * Return: the balance factor
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	return (
+		binary_tree_height(tree->left) -
+		binary_tree_height(tree->right)
+	);
+}
+
+/**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: a pointer to the root node of the tree to measure the height
+ * Return: height but we're doing that weird 0 thing
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (recursiveSearch(tree));
+}
+
+/**
+ * recursiveSearch - recursive function to find height
+ * @tree: current working node
+ * Return: actual height
+ */
+size_t recursiveSearch(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (
+		findMax(
+			recursiveSearch(tree->left),
+			recursiveSearch(tree->right)
+			) + 1
+		);
+}
+
+/**
+ * findMax - find the bigger of two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the larger of the two
+ */
+size_t findMax(size_t a, size_t b)
+{
+	if (a >= b)
+		return (a);
+	else
+		return (b);
+}
